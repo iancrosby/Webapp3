3a03694eccf99a7af1c29da8da7d9712a414dec9
Ian Crosby <ian.crosby@10sheet.com>
2012-12-25 11:25:30 -0800

    Setting up HTML rendering

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-import os
 from flask import Flask, jsonify, render_template, request
 app = Flask(__name__)
 
+@app.route('/_add_numbers')
+def add_numbers():
+    """Add two numbers server side, ridiculous but well..."""
+    a = request.args.get('a', 0, type=int)
+    b = request.args.get('b', 0, type=int)
+    return jsonify(result=a + b)
+
+
 @app.route('/')
-def hello():
-	return 'Hello world!'
-	
+def index():
+    return render_template('index.html')
+
+
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
