194ae5207df5eaa79495258b9ad18fde9e969a4d
Ian Crosby <ian.crosby@10sheet.com>
2012-12-25 11:20:09 -0800

    1st fix attempt

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify, render_template, request
 app = Flask(__name__)
 
@@ -5,4 +6,7 @@ app = Flask(__name__)
 def hello():
 	return 'Hello world!'
 	
-app.run()
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
