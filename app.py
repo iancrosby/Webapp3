f8dc288a82f4298aae098e7dd61f1ba85958ef8f
Ian Crosby <ian.crosby@10sheet.com>
2012-12-25 15:29:06 -0800

    1st attempt to fix HTML rendering

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify, render_template, request
 app = Flask(__name__)
 
@@ -15,4 +16,6 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run()
+	# Bind to PORT if defined, otherwise default to 5000.
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port)
